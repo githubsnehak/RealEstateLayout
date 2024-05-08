d03fae3be024c2d46e97701be07cf9d6b99f331d
githubsnehak <150771569+githubsnehak@users.noreply.github.com>
2024-05-08 14:54:08 +0530

    Update Connect.js

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -16,7 +16,7 @@ const Connect = () => {
               <div className="mb-4">
                 <h1 className="mb-3" style={{ color: "white" }}>Contact With Our Real Estate Advisor</h1>
                 <p>Know more about location, amenities, layouts and prices of Sobha One.</p>
-              </div>
+              </div><br/>
               <a href="" className="btn btn-primary py-3 px-4 me-2"><i className="fa fa-phone-alt me-2"></i>Make A Call</a><br/><br/>
               <a href="" className="btn btn-dark py-3 px-4"><i className="fa fa-calendar-alt me-2"></i>Get Appointment</a>
             </div>
